5ee6af2dcdce3c8aee7e052a87625f8229cadf61
Meta112555 <johannes.lindner@uni-weimar.de>
2014-04-17 12:04:07 +0200

    cleaned

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Johannes on 4/10/14.
- */
-public class Test {
-    public String output ="Hallo";
-    public Test() {
-    }
-}
--- src/martinSchneider/Filter/PixelProcessing.java
+++
@@ -1,9 +0,0 @@
-package martinSchneider.Filter;
-
-/**
- * Created by Johannes on 4/15/14.
- */
-
-public interface PixelProcessing {
-    public int doToPixel(int x, int y, int w, int h, int[] source);
-}
